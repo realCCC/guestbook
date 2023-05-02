28e6476a94d252f0b2c427189c6d383ecdee2183
wpdls2821@gmail.com <wpdls2821@gmail.com>
2023-05-02 16:22:42 +0900

    querydsl1 test

---
+++ src/main/java/kr/ac/kopo/guestbook/repository/GuestbookRepository.java
@@ -0,0 +1,10 @@
+package kr.ac.kopo.guestbook.repository;
+
+import kr.ac.kopo.guestbook.entity.Guestbook;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long>,
+        QuerydslPredicateExecutor<Guestbook> {
+}
