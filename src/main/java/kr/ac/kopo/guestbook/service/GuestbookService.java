1222a195f792090242f99e2bb3b17143d8c9cc5f
wpdls2821@gmail.com <wpdls2821@gmail.com>
2023-05-09 16:33:09 +0900

    1.dto <--> entity 서로 변환 2.repository계층에서 service계층>Controller계층 뷰까지 과정 코딩

---
+++ src/main/java/kr/ac/kopo/guestbook/service/GuestbookService.java
@@ -0,0 +1,36 @@
+package kr.ac.kopo.guestbook.service;
+
+import kr.ac.kopo.guestbook.dto.GuestbookDTO;
+import kr.ac.kopo.guestbook.dto.PageRequestDTO;
+import kr.ac.kopo.guestbook.dto.PageResultDTO;
+import kr.ac.kopo.guestbook.entity.Guestbook;
+
+public interface GuestbookService {
+    Long register(GuestbookDTO dto);
+    PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO);
+
+    // dto를 Entity로 변환!
+    default Guestbook dtoToEntity(GuestbookDTO dto){
+        Guestbook entity = Guestbook.builder()
+                .gno(dto.getGno())
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .writer(dto.getWriter())
+                .build();
+        return entity;
+    }
+
+    // Entity를 dto로 변환!
+    default GuestbookDTO entityTODto(Guestbook entity){
+
+        GuestbookDTO dto = GuestbookDTO.builder()
+                .gno(entity.getGno())
+                .title(entity.getTitle())
+                .content(entity.getContent())
+                .writer(entity.getWriter())
+                .regDate(entity.getRegDate())
+                .modDate(entity.getModDate())
+                .build();
+        return dto;
+    }
+}
---
+++ src/test/java/kr/ac/kopo/guestbook/service/GuestbookServiceTests.java
@@ -0,0 +1,38 @@
+package kr.ac.kopo.guestbook.service;
+
+import kr.ac.kopo.guestbook.dto.GuestbookDTO;
+import kr.ac.kopo.guestbook.dto.PageRequestDTO;
+import kr.ac.kopo.guestbook.dto.PageResultDTO;
+import kr.ac.kopo.guestbook.entity.Guestbook;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+
+@SpringBootTest
+public class GuestbookServiceTests {
+    @Autowired
+    private GuestbookService service;
+
+    @Test
+    public void testRegister(){
+        GuestbookDTO guestbookDTO = GuestbookDTO.builder()
+                .title("Sample Title...")
+                .content("Sample Content...")
+                .writer("userSample")
+                .build();
+        System.out.println(service.register(guestbookDTO));
+    }
+
+    @Test
+    public void testList(){
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .build();
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()){
+            System.out.println(guestbookDTO);
+        }
+    }
+}
